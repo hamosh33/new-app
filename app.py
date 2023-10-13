e932498dc6ec58dbe30808c94683aac213437b2e
hamosh33 <36336354+hamosh33@users.noreply.github.com>
2023-10-14 00:51:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+import telebot
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+TOKEN = '6036880264:AAEe_hX4N-wXmuHUT37X2k-zqkbBXQwsbPo'  # استبدل هذا بتوكن بوتك الخاص
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "مرحبًا، أنا بوت تليجرام الخاص بك!")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
